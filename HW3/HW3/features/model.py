099ffeca72a295ad3c57acbe8d99740b005fbab6
baksben <tatiana.bakwenye@bse.eu>
2023-10-15 08:10:16 +0200

    Changed model function (my version)

--- HW3/HW3/features/model.py
+++ HW3/HW3/features/model.py
@@ -3,27 +3,17 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import roc_auc_score
 
 
-def model(c, d):
-    features = ['age', 'height', 'weight', 'aids', 'cirrhosis', 'hepatic_failure', 'immunosuppression', 'leukemia', 'lymphoma', 'solid_tumor_with_metastasis']
-    target = 'diabetes_mellitus'
-    X = c[features] #train data
-    Y = c[target]
-    Xt = d[features] #test data
-    Yt = d[target]
-    model = RandomForestClassifier(n_estimators=100, random_state=42) 
-    model.fit(X, Y) #to fit the model
-    train_predictions = model.predict_proba(X)[:, 1] #to calculate the prob
-    test_predictions = model.predict_proba(Xt)[:, 1]
-    y_train_pred = model.predict(X) #predict the model
-    y_test_pred = model.predict(Xt)
-    c['train_predictions'] = train_predictions #add the columns predicted
-    d['test_predictions'] = test_predictions
-    train_accuracy = accuracy_score(Y, y_train_pred) #calculates the accuracy of the model
-    test_accuracy = accuracy_score(Yt, y_test_pred)
-    print("Train Accuracy:", train_accuracy)
-    print("Test Accuracy:", test_accuracy)
-    train_roc_auc = roc_auc_score(Y, train_predictions) #runs roc_auc
-    test_roc_auc = roc_auc_score(Yt, test_predictions)
+def model(X_train, X_test, y_train, y_test):
+    model = LogisticRegression()
+    model.fit(X_train, y_train)
+    train_predictions = model.predict_proba(X_train)[:, 1]
+    test_predictions = model.predict_proba(X_test)[:, 1]
+    X_train['predictions'] = train_predictions
+    X_test['predictions'] = test_predictions
+    train_roc_auc = roc_auc_score(y_train, train_predictions)
+    test_roc_auc = roc_auc_score(y_test, test_predictions)
+    
     print("Train ROC AUC:", train_roc_auc)
     print("Test ROC AUC:", test_roc_auc)
-    return c, d
+    
+    return train_roc_auc, test_roc_auc
