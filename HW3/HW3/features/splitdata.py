a76134e816d7ba446448dd64b9b426e9e9e82691
baksben <tatiana.bakwenye@bse.eu>
2023-10-15 08:07:19 +0200

    Changed splitdata function (my version)

--- HW3/HW3/features/splitdata.py
+++ HW3/HW3/features/splitdata.py
@@ -1,8 +1,8 @@
 from sklearn.model_selection import train_test_split
 
 
-def splitdata(x):
-    traindf, testdf = train_test_split(x, test_size=0.4, random_state=42) #takes the data and splits it 40% to test
-    traindf = traindf.reset_index(drop=True) #to reset the index to 0
-    testdf = testdf.reset_index(drop=True)
-    return traindf, testdf
+def splitdata(df):
+    X = df.drop('diabetes_mellitus', axis=1)
+    y = df['diabetes_mellitus']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+    return X_train, X_test, y_train, y_test
