50e7fb0694a9dfb0a1a0dfd2610d3e11a57e6aba
baksben <tatiana.bakwenye@bse.eu>
2023-10-15 08:09:33 +0200

    Added target ad parameter for split function

--- HW3/HW3/features/splitdata.py
+++ HW3/HW3/features/splitdata.py
@@ -1,8 +1,8 @@
 from sklearn.model_selection import train_test_split
 
 
-def splitdata(df):
-    X = df.drop('diabetes_mellitus', axis=1)
-    y = df['diabetes_mellitus']
+def splitdata(df, target_column):
+    X = df.drop(target_column, axis=1)
+    y = df[target_column]
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
     return X_train, X_test, y_train, y_test
