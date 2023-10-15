6f7b16aa7b21282ae0738e9bf8185f62a7827f97
Vanessa Kromm <vanessa.kromm@web.de>
2023-10-15 13:33:44 +0200

    changed names of variables in the function to make it clearer and added parameters fatures and target

--- HW3/HW3_library/features/model.py
+++ HW3/HW3_library/features/model.py
@@ -2,28 +2,26 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import roc_auc_score
 
-
-def model(c, d):
-    features = ['age', 'height', 'weight', 'aids', 'cirrhosis', 'hepatic_failure', 'immunosuppression', 'leukemia', 'lymphoma', 'solid_tumor_with_metastasis']
-    target = 'diabetes_mellitus'
-    X = c[features] #train data
-    Y = c[target]
-    Xt = d[features] #test data
-    Yt = d[target]
+#function that trains a random forrest model
+def model(train_data, test_data, features, target):
+    X_train = train_data[features] #train data
+    Y_train = train_data[target]
+    X_test = test_data[features] #test data
+    Y_test = test_data[target]
     model = RandomForestClassifier(n_estimators=100, random_state=42) 
-    model.fit(X, Y) #to fit the model
-    train_predictions = model.predict_proba(X)[:, 1] #to calculate the prob
-    test_predictions = model.predict_proba(Xt)[:, 1]
-    y_train_pred = model.predict(X) #predict the model
-    y_test_pred = model.predict(Xt)
-    c['train_predictions'] = train_predictions #add the columns predicted
-    d['test_predictions'] = test_predictions
-    train_accuracy = accuracy_score(Y, y_train_pred) #calculates the accuracy of the model
-    test_accuracy = accuracy_score(Yt, y_test_pred)
+    model.fit(X_train, Y_train) #to fit the model
+    train_predictions = model.predict_proba(X_train)[:, 1] #to calculate the prob
+    test_predictions = model.predict_proba(X_test)[:, 1]
+    y_train_pred = model.predict(X_train) #predict the model
+    y_test_pred = model.predict(X_test)
+    train_data['train_predictions'] = train_predictions #add the columns predicted
+    test_data['test_predictions'] = test_predictions
+    train_accuracy = accuracy_score(Y_train, y_train_pred) #calculates the accuracy of the model
+    test_accuracy = accuracy_score(Y_test, y_test_pred)
     print("Train Accuracy:", train_accuracy)
     print("Test Accuracy:", test_accuracy)
-    train_roc_auc = roc_auc_score(Y, train_predictions) #runs roc_auc
-    test_roc_auc = roc_auc_score(Yt, test_predictions)
+    train_roc_auc = roc_auc_score(Y_train, train_predictions) #runs roc_auc
+    test_roc_auc = roc_auc_score(Y_test, test_predictions)
     print("Train ROC AUC:", train_roc_auc)
     print("Test ROC AUC:", test_roc_auc)
-    return c, d
+    return train_data, test_data
