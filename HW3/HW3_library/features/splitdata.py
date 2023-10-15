5264ac3e408a17b3f96669d466a8d6fb69b2ee4b
Vanessa Kromm <vanessa.kromm@web.de>
2023-10-15 13:16:51 +0200

    changed parameters in function and added description

--- HW3/HW3_library/features/splitdata.py
+++ HW3/HW3_library/features/splitdata.py
@@ -1,8 +1,8 @@
 from sklearn.model_selection import train_test_split
 
-
-def splitdata(x):
-    traindf, testdf = train_test_split(x, test_size=0.4, random_state=42) #takes the data and splits it 40% to test
-    traindf = traindf.reset_index(drop=True) #to reset the index to 0
+#function to split the data into train and test data
+def splitdata(df, test_size, random_state):
+    traindf, testdf = train_test_split(df, test_size=test_size, random_state=random_state) 
+    traindf = traindf.reset_index(drop=True) 
     testdf = testdf.reset_index(drop=True)
     return traindf, testdf
